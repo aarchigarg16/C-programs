33da0d747e9477433c91a4f7baf36718b4e43b6b
aarchigarg16 <Aarchi.38900@stu.upes.ac.in>
2026-09-11 14:55:30 +0530

    Add files via upload

---
+++ day1q2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum,diff,prod,quo;
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+    diff=a-b;
+    prod=a*b;
+    quo=a/b;
+    printf("sum=%d\n",sum);
+    printf("diff=%d\n",diff);
+    printf("prod=%d\n",prod);
+    printf("quo=%d\n",quo);
+    return 0;
+}
