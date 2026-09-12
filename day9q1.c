673d2cbc767cd1ec06169dde54d598883d585cdc
aarchigarg16 <Aarchi.38900@stu.upes.ac.in>
2026-09-12 19:30:13 +0530

    Add files via upload

---
+++ day9q1.c
@@ -0,0 +1,39 @@
+//Q17: Write a program to find the roots of a quadratic equation and categorize them.
+
+/*
+Sample Test Cases:
+Input 1:
+1 -3 2
+Output 1:
+Roots are real and different: 2, 1
+
+Input 2:
+1 -2 1
+Output 2:
+Roots are real and same: 1
+
+Input 3:
+1 2 5
+Output 3:
+Roots are complex
+
+*/
+
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    int d,r1,r2;
+    d=b*b-4*a*c;
+    r1= (-b+sqrt(d))/2*a;
+    r2=(-b-sqrt(d))/2*a;
+    if(d>0){
+        printf("roots are real and different %d %d",r1,r2);
+    }
+    else if(d==0)
+    printf("roots are real and same %d %d",r1,r2);
+    else
+    printf("roots are complex");
+    return 0;
+}
