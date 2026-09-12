f92c9c489150e367c02c8429189b4a783c52834a
aarchigarg16 <Aarchi.38900@stu.upes.ac.in>
2026-09-13 02:05:20 +0530

    Add files via upload

---
+++ day15q2.c
@@ -0,0 +1,27 @@
+//Q30: Write a program to reverse a given number.
+
+/*
+Sample Test Cases:
+Input 1:
+1234
+Output 1:
+4321
+
+Input 2:
+100
+Output 2:
+1
+
+*/
+
+#include<stdio.h>
+int main(){
+   int n,r;
+   scanf("%d",&n);
+   while(n!=0){
+      r=n%10;
+      printf("%d",r);
+      n=n/10;
+   }
+   return 0;
+}
