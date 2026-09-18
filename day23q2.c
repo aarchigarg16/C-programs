0b499a57d61082a3446b4cf4a52efc5d5c031583
aarchigarg16 <Aarchi.38900@stu.upes.ac.in>
2026-09-18 22:22:48 +0530

    Add files via upload

---
+++ day23q2.c
@@ -0,0 +1,31 @@
+//Q46: Write a program to print the following pattern:
+/******
+*****
+*****
+*****
+*****
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+*****
+*****
+*****
+*****
+
+*/
+
+#include<stdio.h>
+int main(){
+    int i,j,n=5;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
